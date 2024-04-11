b23e24e739346da2c3a1564b9ec37495d824fa20
jorgevendramini <jorgemunhoz_88@hotmail.com>
2024-04-10 22:26:58 -0300

    almost done

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Especialidade(models.Model): #JorgeEnrique
+    id_especialidade = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+class Medico(models.Model):
+    id_medico = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    data_nascimento = models.DateField()
+    crm = models.CharField(max_length=20)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE)
+    
+#JorgeEnrique
